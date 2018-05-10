1090079bdfae02f73eec155af2062151e4f36910
ronaldo faustino <ohnirucse@msn.com>
2018-05-10 17:00:08 -0300

    Complementando Codigo

--- exemplos.rb
+++ exemplos.rb
@@ -1,14 +1,22 @@
-puts 175 == 175
-puts 175 == 174
-puts "175" == 175
-puts "175" == "175"
-puts "175".to_i == 175
+# puts 175 == 175
+# puts 175 == 174
+# puts "175" == 175
+# puts "175" == "175"
+# puts "175".to_i == 175
 
-puts
+# puts
 
-puts 175 != 174
-puts 175 != 175
-puts 175 > 174
-puts 175 >= 175
-puts 175 < 176
-puts 175 <= 175
+# puts 175 != 174
+# puts 175 != 175
+# puts 175 > 174
+# puts 175 >= 175
+# puts 175 < 176
+# puts 175 <= 175
+
+
+
+for contador in 1..4
+    puts "Numero "+ contador.to_s 
+    
+end
+
--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,23 +1,40 @@
-puts "Bem Vindo ao Jogo da Adivinhação"
-puts "\n"
-puts "Qual é o seu nome?"
-nome = gets
-puts "Vamos começar o jogo para você #{nome}"
-puts "\n"
+def da_boas_vindas
+    puts "Bem Vindo ao Jogo da Adivinhação"
+    puts "\n"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "Vamos começar o jogo para você #{nome}"
+    puts "\n"
+end
+
+da_boas_vindas
+
 puts "Escolha um numero de 0 a 200"
 numero_secreto = 175
 puts "Escolheu o numero, que tal adivinhar nosso numero secreto."
 puts "\n\n"
-puts "1 tentativa, digite o numero"
+
+limite_de_tentativas = 5
+
+for tentativa in 1.. limite_de_tentativas
+
+puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s 
 chute = gets
 puts "Será que você acertou? O numero que você chutou foi " + chute
-
+puts "\n"
 acertou = numero_secreto == chute.to_i
 
 if acertou
     puts "Parabéns você acertou!"
+    break
     else 
-    puts "Vamos tentar novamente!"
+        maior = numero_secreto > chute.to_i
+        if maior
+            puts "O número é maior"
+        else
+            puts "O número é menor"
+        end
+end
 end
 
 #2.8 2
